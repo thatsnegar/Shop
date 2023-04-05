1453f59a85f4ea0c89921ca27b837ba5869186d0
Negar.Abdi <Negar.Abdi@hotmail.com>
2023-04-05 05:22:06 -0700

    Add project files.

---
+++ OnlineShop/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
